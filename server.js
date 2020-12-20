665b5ae4fbd0561ab9bad067cce973184a8ddd38
YuriyWebSpinner <myEmail@example.com>
2020-12-20 11:12:43 +0300

    fix server

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const { createServer } = require('http')
+const { parse } = require('url')
 const next = require('next')
 
-const port = 3000
 const dev = 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = new URL(req.url, 'http://151.248.121.23')
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
     const { pathname, query } = parsedUrl
 
     if (pathname === '/a') {
@@ -18,8 +20,8 @@ app.prepare().then(() => {
     } else {
       handle(req, res, parsedUrl)
     }
-  }).listen(port, (err) => {
+  }).listen(3000, (err) => {
     if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
+    console.log('> Ready on http://localhost:3000')
   })
 })
